96a2aa3626470500d61a48a4cf69159b424de316
Yuri Trikoz <ytrikoz@gmail.com>
2020-12-06 22:09:54 +0300

    updates and comments

--- include/consts.h
+++ include/consts.h
@@ -2,6 +2,8 @@
 
 #define ONE_SECOND 1000
 
+// Время опроса состояния ручки собития произошедше в интервале меньше указанного будут проигорированры
 #define CONTROL_INTERVAL_ms 50
-#define LED_LIGHT_TIME_ms 3 * ONE_SECOND
+// Время свечения светодида 
+#define LED_LIGHT_TIME_ms ONE_SECOND
 
