237c45c24c7a394db22f49b9814481277ce9582b
janvarry <janvarry@gmail.com>
2020-12-22 23:56:06 +0300

    update

--- README.md
+++ README.md
@@ -7,20 +7,20 @@ Project based on capturing part of warnings,OCR - it , and control over nodemcu 
 
 
 ```Cruise control switch circuit.jpg```  Rav4 schematic cruise control regulator
-
-
+```termux.apk``` Termux installer for android
+```com.termux.api-20201222-204040.TiBkp``` Titanium Backup of termux API
 ```resistor.jpg```  How to connect cruise control regulator to nodemcu/arduino, does not matter
-
-
 ```schematic.jpg``` principial schematic of rav4 - (2004 aca20) -testing - cruise control wiring
-
-
 ```tester.ino```  - arduino/nodemcu test project to get real values for cruise control regulator(different resistance)(with resistor 2kOm) 
 (unknown resistor is a "cruise control regulator" ,from the car)
-
-
 ```transistor1.jpg```  - principial wiring for chip to make cruise control
-
+```org.prowl.torque-20201222-200422.TiBkp``` Titanium backup of Torque - for speed receiving from car
+```obd2-simulator.zip``` OBD2 Simulator for PC+bluetooth
+Pair your android device with PC over bluetooth
+Check which bluetooth com-port are using, for me is COM16..
+run:obdsim.exe -w COM16
+Start torque application and connect to bluetooth elm , which is your pc.
+Play with "speed", in torque speed must changes!
 
 
 ```#install termux on android
@@ -36,9 +36,6 @@ apt install  tesseract
 export termux_bin_path="/data/data/com.termux/files/usr/bin"; export owner="$(stat -c "%u" "$termux_bin_path")"; for f in tesseract; do if [ -f "$termux_bin_path/$f" ]; then su -c "chown $owner:$owner \"$termux_bin_path/$f\" && chmod 700 \"$termux_bin_path/$f\""; fi; done;
 ```
 
-
-##  For current speed using : gplogger (added)
-##  It is possibble to read speed from elm327 by bluetooth
 
 
 |#######################________| Processing of project.....
--- python/android.py
+++ python/android.py
@@ -1,13 +1,20 @@
 import time
+import keyboard
 from threading import Thread
 import threading
+import re
 from re import search
 from time import sleep
 import os
 import sys
 import telnetlib
 th = ""
+overspeed = 15 #Speed which can be overvalued
 lol = 0
+iter = 0
+restart = 0
+lastdate = ""
+lastdatex = ""
 tn_ip = "192.168.4.1"
 tn_port = "23"
 data = ""
@@ -16,26 +23,37 @@ up = "#UPOK#"
 down = "#DOWNOK#"
 cancel = "#CANCELOK#"
 onoff = "#ONOFFOK#"
+selfup = "#SELF_U#"
+selfdown = "#SELF_D#"
+selfcancel = "#SELF_C#"
+selfonoff = "#SELF_SW#"
+
 connected = False
-active = True
+active = False
 diff = "0"
 slow = ""
 limited = ""
 gps = ""
-
+dosleep = False
+s = 0
 
 def senddatadown(diff):
     number = 1
+    global dosleep
     d1 = "DOWN\r\n"
     while (number <= diff):
         tn.write(d1.encode())
         time.sleep(0.4)
         print (d1)
         number = number + 1
-        print ("We just down")
+        print ("We just speed down")
+    print ("We just speed down and SLEEP")
+    time.sleep (5)
+    dosleep = True
 
 
 def senddataup(diff):
+    global dosleep
     number = 1
     d1 = "UP\r\n"
     print ("senddataup")
@@ -44,85 +62,165 @@ def senddataup(diff):
         time.sleep(0.4)
         print (d1)
         number = number + 1
-        print ("We just down")
+        print ("We just speed up")
+    print ("We just speed UP and SLEEP")
+    time.sleep (5)
+    dosleep = True
 
 
 def getspeeds():
     global active
     global gps
     global limited
+    global lastdate
+    global lastdatex
+    global iter
+    global dosleep
+    global restart
+    if active is False and (restart >0 and restart < 3):
+        try:
+            os.popen("killall -9 ru.yandex.yandexnavi").read
+            time.sleep(1)
+            os.popen("am start -W --activity-brought-to-front --user 0 -n ru.yandex.yandexnavi/ru.yandex.yandexnavi.core.NavigatorActivity").read()
+            time.sleep(5)
+            os.popen("killall -9 org.prowl.torque").read
+            time.sleep(1)
+            os.popen("am start -W  --user 0 -n org.prowl.torque/org.prowl.torque.landing.FrontPage").read()
+            time.sleep(5)
+            restart = restart + 1
+            active = True
+        except:
+            time.sleep(1)
+            pass
+
     if active is True:
+        if dosleep is True:
+            time.sleep(8)
+            dosleep = False
         try:
             gps = \
                 os.popen(
-            #        "tail -1  /data/data/com.termux/files/home/storage/shared/Android/data/com.mendhak.gpslogger/files/gpslogger.csv |awk -F, '{print $7}'|awk -F. '{print $1}'").read()
-                     "ps -A|grep -q -i com.mendhak.gpslogger || am start --user 0 -n com.mendhak.gpslogger/com.mendhak.gpslogger.GpsMainActivity && tail -1  /data/data/com.termux/files/home/storage/shared/Android/data/com.mendhak.gpslogger/files/gpslogger.csv |awk -F, '{print $7}'|awk -F. '{print $1}'").read()
-            # run gps "am start --user 0 -n com.mendhak.gpslogger/com.mendhak.gpslogger.GpsMainActivity"
-            # "ps -A|grep -q -i com.mendhak.gpslogger || am start --user 0 -n com.mendhak.gpslogger/com.mendhak.gpslogger.GpsMainActivity && tail -1  /data/data/com.termux/files/home/storage/shared/Android/data/com.mendhak.gpslogger/files/gpslogger.csv |awk -F, '{print $7}'|awk -F. '{print $1}'").read()
-            print ("We req gps")
-            gps = gps.replace("\r", "")
+                     "ps -A|grep -q -i org.prowl.torque||am start -W --activity-brought-to-front --user 0 -n org.prowl.torque/org.prowl.torque.landing.FrontPage&&cat  /sdcard/torqueLogs/* |tail -1").read()
+            print ("We reqire obd2 speed")
+            gps1 = re.split(r',', gps)
+            lastdate=gps1[0] #date
+            spd=gps1[1] #speed
+            gps = spd.replace("\r", "")
             gps = gps.replace("\n", "")
-            #print (gps)
-	    if not (gps==int(gps)):
+            print ("This is obd2 speed")
+            print (gps)
+            if (lastdate in lastdatex):
+                print ("last date of obd is same")
+                print (lastdate)
+                print (lastdatex)
+                if (iter < 20):
+                    iter = iter + 1
+                    print("iter is lower than 10,iter value is")
+                    print(iter)
+                    time.sleep(2)
+                else:
+                    print("iter value is more than 10")
+                    print(iter)
+                    print("We set active to off")
+                    active = False
+                    restart = restart + 1
+            else:
+                print ("last date of obd is different")
+                print (lastdate)
+                print (lastdatex)
+                lastdatex = lastdate
+                restart = 0
+                iter = 0
+
+            print ("obd speed ")
+            print (int(gps))
+            if isinstance(int(gps),str):
                 gps = 0
-	    else:
-                print(str(gps))
+                print ("obd speed is not valid")
+                print("we set gps 0")
+                print (str(gps))
+            else:
+                if int(gps) >180 or int(gps) < 36:
+                    gps = 0
+                    print ("limit is not valid,(exceed speed >180 or <36")
+                    print("we set gps 0")
+                    print (str(gps))
         except:
             gps = 0
             time.sleep(5)
+            print("we got exception")
             pass
 
         try:
             limited = \
-                os.popen(
-                   # "ps -A|grep -q -i ru.yandex.yandexnavi && (/system/bin/screencap -p /data/data/com.termux/files/home/map.png && (/data/data/com.termux/files/usr/bin/mogrify -crop '125x100+910+140' -set units PixelsPerInch -density 300 -write  /data/data/com.termux/files/home/map_.png  /data/data/com.termux/files/home/map.png && (/data/data/com.termux/files/usr/bin/tesseract --oem 0 -c tessedit_char_whitelist=0123456789  /data/data/com.termux/files/home/map_.png /data/data/com.termux/files/home/map1 > /dev/null 2>&1 &&head  -1 /data/data/com.termux/files/home/map1.txt)) )"
-                   #Yandex speed limits
-                    "ps -A|grep -q -i info.mapcam.droid ||am start --user 0 -n info.mapcam.droid/info.mapcam.droid.MainActivity && (/system/bin/screencap -p /data/data/com.termux/files/home/map.png && (/data/data/com.termux/files/usr/bin/mogrify -crop '200x140+340+700' -set units PixelsPerInch -density 300 -write  /data/data/com.termux/files/home/map_.png  /data/data/com.termux/files/home/map.png && (/data/data/com.termux/files/usr/bin/tesseract --oem 0 -c tessedit_char_whitelist=0123456789  /data/data/com.termux/files/home/map_.png /data/data/com.termux/files/home/map1 > /dev/null 2>&1 &&head  -1 /data/data/com.termux/files/home/map1.txt)) )"
-                   #convert  /data/data/com.termux/files/home/storage/shared/mapx.png  +dither -colors 5 -unique-colors txt:
-                   #FAFAFAFF
-		   #Mapcaminfo speed limits
-		   # info.mapcam.droid
-		   # am start --user 0 -n info.mapcam.droid/info.mapcam.droid.MainActivity
-			
-                ).read()
-            print ("We req limit")
+                os.popen("if ps -A|grep -q -i ru.yandex.yandexnavi; then if /system/bin/screencap -p /data/data/com.termux/files/home/map.png && (/data/data/com.termux/files/usr/bin/mogrify -crop '125x100+910+140' -set units PixelsPerInch -density 300 -write  /data/data/com.termux/files/home/map_.png  /data/data/com.termux/files/home/map.png && convert /data/data/com.termux/files/home/map_.png -define histogram:unique-colors=true -format %c histogram:info:-|grep FE5858 -i -q ); then if [ $(convert /data/data/com.termux/files/home/map_.png -define histogram:unique-colors=true -format %c histogram:info:-|grep FE5858 |tr -d ' '|cut -f1 -d ':' ) -ge 500 ]; then /data/data/com.termux/files/usr/bin/tesseract --oem 0 -c tessedit_char_whitelist=0123456789  /data/data/com.termux/files/home/map_.png /data/data/com.termux/files/home/map1 > /dev/null 2>&1 && head  -1 /data/data/com.termux/files/home/map1.txt; 	else  echo 0;	fi else  echo 0;  fi fi ").read()
+            print ("We reqire limit")
             limited = limited.replace("\r", "")
             limited = limited.replace("\n", "")
+            print ("This is limit")
             print (limited)
-	    if not (limited==int(limited)):
+            if isinstance(int(limited),str):
                 limited = 0
+                print ("limit is not valid")
                 print (str(limited))
+            else:
+                if int(limited) >180 or int(limited) < 36:
+                    limited = 0
+                    print ("limit is not valid,(exceed speed >180 or <36)")
+                    print (str(limited))
         except:
             limited = 0
             pass
 
 def check():
+    global overspeed
     global gps
     global limited
     getspeeds()
     number = 1
-    if (gps == 0) or (limited == 0):
+#//    if (gps or gps==0) or (limited or limited==0) :
+    if gps==0 or limited==0 :
+#        print("gps or limited is 0")
+####    if (gps or gps==0) :
+#    if (gps == 0) or (limited == 0):
         return None
+####################
+    limited = int(limited) + int(overspeed)
+    print ("This is limit with overspeed")
+    print (limited)
+
     if (int(limited) < int(gps)):
+##        gps = (int(gps) )
         gps = (int(gps))
         limited = (int(limited))
         if (gps - limited) == 1:
             diff = ((gps - limited))
         else:
-            diff = ((gps - limited) / 2)
-        print (int(diff))
-        print ("we in limited lower gps")
+            if ((gps - limited) % 2 != 0):
+                print ("not devided")
+                diff = ((((gps - limited) + 1) / 2) * 2)
+                print (diff)
+            else:
+                diff = (((gps - limited) / 2 )* 2 )
+        print ("This is diff", int(diff))
+#        print (int(diff))
+        print ("We in limited lower gps")
         senddatadown(int(diff))
-    elif (int(limited) > int(gps)):
+    elif (int(limited) > (int(gps)+5)):
         gps = (int(gps))
         limited = (int(limited))
         if (gps - limited) == 1:
             diff = ((gps - limited))
         else:
-            diff = ((gps - limited) / 2)
-        print (int(diff))
-        print ("we in limited more gps")
-        senddataup(int(diff))
+            if ((gps - limited) % 2 != 0):
+                print ("not devided")
+                diff = ((((gps - limited) + 1) / 2) * 2 )
+                print (abs(diff))
+            else:
+                diff = (((gps - limited) / 2) * 2 )
+#        print (abs(int(diff)))
+        print ("we in limited more gps", abs(int(diff)))
+        senddataup(abs(int(diff)))
 
 
 def connect():
@@ -136,8 +234,8 @@ def connect():
             print ("Connected")
             if (lol <1):
                 print ("Starting thread")
-            	th = threading.Thread(target=run)
-            	th.start()
+                th = threading.Thread(target=run)
+                th.start()
                 lol = lol + 1
             if th.is_alive() is False:
                 th = threading.Thread(target=run)
@@ -158,9 +256,16 @@ def run():
     global gps
     global limited
     global active
+    global iter
     print ("We are in main loop")
     try:
         while True:
+            try:  
+                if keyboard.is_pressed('q'):
+                    print('Exit!')
+                    break 
+            except:
+                    pass    
             data=''
             while data.find('#'):  # == -1:
                 data = tn.read_very_eager()
@@ -173,7 +278,11 @@ def run():
                     print ("\r\nACTIVE AFTER " + str(active) + "\r\n")
                 elif active == False:
                     active = True
+                    iter = 0
+                    d1 = "DOWN\r\n"
+                    tn.write(d1.encode())
                     print ("\r\nACTIVE AFTER " + str(active) + "\r\n")
+                    print ("\r\nWe sended activate Cruise control "
             elif (up in data):
                 print ("We found " + up)
             elif (down in data):
@@ -182,11 +291,38 @@ def run():
                 print ("We found " + cancel)
             elif (onoff in data):
                 print ("We found " + onoff)
+            elif (selfup in data):
+                print ("We found " + selfup)
+            elif (selfdown in data):
+                print ("We found " + selfdown)
+            elif (selfcancel in data):
+                print ("We found " + selfcancel)
+            elif (selfonoff in data):
+                print ("We found " + selfonoff)
             else:
                 print ("Try again")
     except IOError:
         print ("Connection Lost")
         connected = False
 
+if s==0:
+    gps = 0
+    limited = 0
+    os.popen("ip rule add prio 100 from all lookup 100").read
+    os.popen("ip route add table 100 192.168.4.0/24 dev wlan0").read
+    os.popen("ip route add table 100 192.168.1.0/24 dev wlan0").read
+    os.popen("killall -9 org.prowl.torque").read    
+    time.sleep(3)
+#    os.popen("killall -9 ru.yandex.yandexnavi").read
+#    time.sleep(1)
+#    os.popen("am start -W --activity-brought-to-front --user 0 -n info.mapcam.droid/info.mapcam.droid.MainActivity").read()
+#    os.popen("am start -W --activity-brought-to-front --user 0 -n OCTech.Mobile.Applications.TouchScan/octech.mobile.applications.touchscan.MainActivity").read()
+###    os.popen("am start -W --activity-brought-to-front --user 0 -n org.prowl.torque/org.prowl.torque.landing.FrontPage").read()
+    os.popen("am start -W --user 0 -n org.prowl.torque/org.prowl.torque.landing.FrontPage&").read()
+    time.sleep(5)
+    os.popen("am start --activity-brought-to-front --user 0 -n ru.yandex.yandexnavi/ru.yandex.yandexnavi.core.NavigatorActivity").read()
+    time.sleep(5)
+    s = 1
+    dosleep = True
 
 connect()
